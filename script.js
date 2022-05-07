81285b0d43d7b90077510f42e5674aeeeea9b868
John Doe <johndoe@example.com>
2022-05-07 11:34:00 +0200

    done

--- script.js
+++ script.js
@@ -19,24 +19,12 @@ planeImg.src = 'images/plane.png';
 const policeCarImg = new Image();
 policeCarImg.src = 'images/policeCar.png';
 
-const burnningSun = new Image();
-burnningSun.src = 'images/burnningSun.jpeg'
-
-
-//window.onload making all the elements invisible...
-
-window.onload = function () {
-    ctx.drawImage(hospitalImg, hospital.x, hospital.y, hospital.width, hospital.height);
-    ctx.drawImage(ambulanceImg, ambulance.x, ambulance.y, ambulance.width, ambulance.height);
-    ctx.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height);
-    ctx.drawImage(policeCarImg, policeCar.x, policeCar.y, policeCar.width, policeCar.height);
-}
-
-
+const moon = new Image();
+moon.src = 'images/moon.png'
 
 const plane = {
-    x: 200,
-    y: 10,
+    x: 20,
+    y: 191,
     width: 150,
     height: 120
 }
@@ -62,11 +50,45 @@ const policeCar = {
     width: 100,
     height: 90
 }
-// 
-
-function moveVehiclesOnGround() {
-    policeCar.x -= 10; 
-    ambulance.x -=10;
-    ctx.drawImage(policeCarImg, policeCar.x, policeCar.y, policeCar.width, policeCar.height);
-    ctx.drawImage(ambulanceImg, ambulance.x, ambulance.y, ambulance.width, ambulance.height);
+const sun = {
+    x: 260,
+    y: 10,
+    width: 160,
+    height: 190
 }
+
+window.onload = function () {
+    ctx.drawImage(hospitalImg, hospital.x, hospital.y, hospital.width, hospital.height);
+    ctx.drawImage(ambulanceImg, ambulance.x, ambulance.y, ambulance.width, ambulance.height);
+    ctx.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height);
+    ctx.drawImage(policeCarImg, policeCar.x, policeCar.y, policeCar.width, policeCar.height);
+    ctx.drawImage(moon, sun.x, sun.y, sun.height, sun.width );
+}
+
+function moveVehicles() {
+    ctx.clearRect(0, 0, 1000, 600);
+    policeCar.x = policeCar.x -10; 
+    ambulance.x = ambulance.x -10;
+    plane.x = plane.x +10;
+    plane.y = plane.y -3;
+
+    if(policeCar.x && ambulance.x <0){
+        policeCar.x = +840;
+        ambulance.x = +1000;
+    }
+    if (plane.x > 900){
+        plane.y = 191;
+        plane.x = 1;
+    }
+
+
+    ctx.drawImage(hospitalImg, hospital.x, hospital.y, hospital.width, hospital.height);
+    ctx.drawImage(ambulanceImg, ambulance.x, ambulance.y, ambulance.width, ambulance.height);
+    ctx.drawImage(policeCarImg, policeCar.x, policeCar.y, policeCar.width, policeCar.height);
+    ctx.drawImage(moon, sun.x, sun.y, sun.height, sun.width );
+    ctx.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height);
+}
+
+setTimeout(function(){
+    let interval = setInterval(moveVehicles, 100)
+}, 1000)
